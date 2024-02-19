60f7c58634c63d9e7ffbe43393cf44bee0d2324a
Albert Guo <guomaoxu0@gmail.com>
2024-02-19 09:11:58 +0800

    fix: theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,32 +1,8 @@
 local M = {
   {
-    'uloco/bluloco.nvim',
-    lazy = false,
-    priority = 1000,
-    dependencies = { 'rktjmp/lush.nvim' },
-    opt = {
-      style = "auto",               -- "auto" | "dark" | "light"
-      transparent = false,
-      italics = false,
-      terminal = vim.fn.has("gui_running") == 1, -- bluoco colors are enabled in gui terminals per default.
-      guicursor   = true,
-    },
-  },
-  {
     'sainnhe/everforest',
     lazy = false,
     priority = 1000,
-  },
-  {
-    'projekt0n/github-nvim-theme',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require('github-theme').setup({
-        -- ...
-      })
-
-    end,
   },
 }
 
