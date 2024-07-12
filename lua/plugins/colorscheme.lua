5f18d3711d5992ee15f6e37f29f1349c2cc36d74
Albert Guo <guomaoxu0@gmail.com>
2024-07-12 09:55:00 +0800

    feat: auto dart, not enable

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,32 @@
 return {
+  "f-person/auto-dark-mode.nvim",
+  opts = {
+    update_interval = 1000,
+    set_dark_mode = function()
+      vim.api.nvim_set_option("background", "dark")
+      vim.cmd("colorscheme everforest")
+    end,
+    set_light_mode = function()
+      vim.api.nvim_set_option("background", "light")
+      vim.cmd("colorscheme everforest")
+    end,
+  },
   {
     "neanias/everforest-nvim",
-    version = false,
     lazy = false,
+    version = false,
     priority = 1000, -- make sure to load this before all the other start plugins
+    main = "everforest",
     -- Optional; default configuration will be used if setup isn't called.
-    config = function()
-      require("everforest").setup({
-        background = "soft",
-      })
-    vim.opt.background = "light"
-    vim.cmd([[colorscheme everforest]])
+    opts ={ 
+      background = "soft",
+      },
+    config = function(_, opts)
+      require("everforest").setup(opts)
+      vim.opt.background = "dark"
+      vim.cmd([[colorscheme everforest]])
     end,
+
+
   },
 }
